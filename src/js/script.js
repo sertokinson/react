70145439ac70d7deef76ad53b2e2b831f2a041a4
Sergey Stakhanov <sertokinson@gmail.com>
2019-03-25 20:35:21 +0300

    Жизненный цикл компонента

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,59 @@
-class HelloWorld extends  React.Component{
-    render(){
+class HelloWorld extends React.Component {
+    render() {
         return <h1>Hello {this.props.name}</h1>
     }
 }
-class Clock extends React.Component{
-    constructor(props){
+
+class Clock extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            time:(new Date).toLocaleTimeString()
+        this.state = {
+            time: (new Date).toLocaleTimeString()
         };
+
+    }
+    componentDidMount(){
         this.updateTime();
     }
-    updateTime(){
-        setInterval(()=>{
+
+    updateTime() {
+        let interval = setInterval(() => {
             this.setState({
-                time:(new Date).toLocaleTimeString()
+                time: (new Date).toLocaleTimeString()
             })
-        },1000)
+        }, 1000);
+        this.setState({interval})
     }
-    render(){
-        return <h1>{this.state.time}</h1>
+
+    componentWillUnmount(){
+        clearInterval(this.state.interval)
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.time}</h1>
+                <p>Осталось: {this.props.second}</p>
+            </div>
+        )
     }
 }
 
-ReactDOM.render(
-    <Clock />,
-    document.getElementById('content')
-);
+let second = 5;
+setInterval(() => {
+    if (second === 0) {
+        ReactDOM.render(
+            <div>
+                <p>Clock has been removed!</p>
+            </div>,
+            document.getElementById('content')
+        )
+    }
+    else {
+        ReactDOM.render(
+            <Clock second={second}/>,
+            document.getElementById('content')
+        );
+        second -= 1;
+    }
+}, 1000);
